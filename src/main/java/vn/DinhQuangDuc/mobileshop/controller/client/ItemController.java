45fd61946bd40c0fa9aacd2aac4c5b4b4aa325b7
Yuno2204 <dqduc2204@gmail.com>
2026-04-23 16:07:09 +0700

    Chức năng chi tiết Giỏ hàng

--- src/main/java/vn/DinhQuangDuc/mobileshop/controller/client/ItemController.java
+++ src/main/java/vn/DinhQuangDuc/mobileshop/controller/client/ItemController.java
@@ -1,11 +1,16 @@
 package vn.DinhQuangDuc.mobileshop.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.DinhQuangDuc.mobileshop.domain.Cart;
+import vn.DinhQuangDuc.mobileshop.domain.CartDetail;
 import vn.DinhQuangDuc.mobileshop.domain.Product;
+import vn.DinhQuangDuc.mobileshop.domain.User;
 import vn.DinhQuangDuc.mobileshop.service.ProductService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +46,19 @@ public class ItemController {
     }
 
     @GetMapping("/cart")
-    public String getCartPage(Model model) {
+    public String getCartPage(Model model, HttpServletRequest request) {
+        User currentUser = new User();
+        HttpSession session = request.getSession(false);
+        long id = (long) session.getAttribute("id");
+        currentUser.setId(id);
+        Cart cart = this.productService.fetchByUser(currentUser);
+        List<CartDetail> cartDetails = cart.getCartDetails();
+        double totalPrice = 0;
+        for (CartDetail cd : cartDetails) {
+            totalPrice += cd.getPrice() * cd.getQuantity();
+        }
+        model.addAttribute("cartDetails", cartDetails);
+        model.addAttribute("totalPrice", totalPrice);
         return "client/cart/show";
     }
 
